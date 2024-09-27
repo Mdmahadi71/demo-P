23eefed190ee1222d997592478e15c1f4300c3a7
Mdmahadi71 <habibalmasud79@gmail.com>
2024-09-27 06:45:41 -0700

    demo

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -11,10 +11,10 @@ const Banner = () => {
 
     }
     return (
-        <div className=' py-[50px]'>
+        <div className=' py-[50px] bg-black'>
             <div className=" container">
-                <div className=" flex justify-between gap-x-[20px] items-center">
-                    <div className="w-[40%]">
+                <div className=" lg:flex justify-between gap-x-[20px] items-center">
+                    <div className="lg:w-[40%]">
                         <div className=" border-[2px] border-black text-center py-[40px] px-[10px] rounded-tl-[100px] rounded-br-[100px] bg-[#00FFFF]">
 
                             <div className=" w-[200px] h-[200px] mx-auto border-[5px] border-black rounded-[50%] 
@@ -34,17 +34,17 @@ const Banner = () => {
                                 <h3 className=' font-openG font-medium text-[15px] text-blue-500'>Dhaka, Bangladesh</h3>
                             </div>
                           
-                            <button className=" flex justify-center mx-auto gap-x-2 items-center py-2 px-[16px] bg-Sky  rounded-[5px] my-[10px] ">
+                            <button className=" flex justify-center mx-auto gap-x-2 items-center py-2 px-[16px] bg-Sky hover:bg-black text-black  hover:text-white duration-300 rounded-[5px] my-[10px] ">
                                 <BsDownload />
-                                <h3 className=' font-openG font-medium text-[17px] text-black'>Download CV </h3>
+                                <h3 className=' font-openG font-medium text-[17px]'>Download CV </h3>
                             </button>
                          
                         </div>
                     </div>
-                    <div className="w-[65%]">
+                    <div className="lg:w-[65%] my-4 lg:my-0">
                         <div className="">
-                            <h4 className=' font-openG font-bold text-[32px] '>Hi</h4>
-                           <h2 className=' font-openG font-semibold text-[22px] text- '>Lorem ipsum dolor sit amet.</h2>
+                            <h4 className=' font-openG font-bold text-[32px] text-Sky'>Hi</h4>
+                           <h2 className=' font-openG font-semibold text-[22px] text-Sky '>Lorem ipsum dolor sit amet.</h2>
                             <h1 style={{paddingTop: '1rem', margin: 'auto 0', fontWeight: 'bold'  }}>
                                 {' '}
                                 <span style={{color: '#0ea5e9', fontWeight: 'bold', fontFamily: "Sedan",  }}>
@@ -62,10 +62,12 @@ const Banner = () => {
                                     />
                                 </span>
                             </h1>
-                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem non maxime quis quos, 
+                            <h2 className=' font-openG font-normal text-[16px] text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem non maxime quis quos, 
                                 laboriosam harum unde consequatur voluptas quasi iste sed cumque rem quas odit cupiditate incidunt commodi quam! 
                                 Veniam magni, ipsa natus ratione molestiae laboriosam 
                                 fuga quis dignissimos dicta quod illum voluptas iure ab officia eum illo voluptate nemo.</h2>
+
+                            <button className=' py-2 px-4 bg-[#00FFFF] font-openG font-semibold text-[20px] text-black rounded-[5px] my-3'>Read more</button>
                         </div>
                     </div>
                 </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,14 @@ import { HiMiniBarsArrowDown ,HiMiniBarsArrowUp } from "react-icons/hi2";
 const Navbar = () => {
     let [show,setshow] =useState(false)
     return (
-        <div className={`lg:py-[15px] py-[20px] bg-black lg:h-auto duration-300 ${show == true ? ' min-h-[250px]' : ''}`}>
+        <div className={`lg:py-[15px] py-[20px] bg-black lg:h-auto  lg:border-b-[2px] border-white duration-300  ${show == true ? ' min-h-[250px]' : ''}`}>
             <div className=" container">
                 <div className=" flex justify-between items-center py-[30px] lg:py-0 ">
                     <div className={`w-[15%] lg:static cursor-pointer absolute duration-300 ${show ==true ? 'top-[200px]' :' top-[30px]'}`}>
                         <h2 className=' font-openG font-bold text-[22px] text-white '>LOREM</h2>
                     </div>
 
-                    <div className={`w-[85%] lg:flex justify-between items-center  absolute lg:static duration-300 ${show==true ? ' top-0 right-0 bg-[red] w-full ' :' top-[-300px] right-0 w-full '}`}>
+                    <div className={`w-[85%] lg:flex justify-between items-center  absolute lg:static duration-300 ${show==true ? ' top-0 right-0 bg-black w-full ' :' top-[-300px] right-0 w-full '}`}>
                         <div className="lg:w-[70%] ">
                             <ul className=' lg:flex lg:gap-x-[40px] justify-center my-4 lg:my-0 text-center lg:text-start'>
                                 <li className=' font-openG font-semibold text-[18px] text-white hover:text-Sky duration-300'>Home</li>
@@ -21,7 +21,7 @@ const Navbar = () => {
                             </ul>
                         </div>
 
-                        <div className="lg:w-[15%] text-center">
+                        <div className="lg:w-[15%] text-center lg:my-0 my-3">
                             <button className=' px-[15px] py-[8px] bg-Sky font-openG font-medium text-[17px] text-white hover:bg-blue-800 duration-300 rounded-[5px] '>Hare Me</button>
                         </div>
                     </div>
